c22b6051362d7665f1373493031ba97d00e68218
김흥기 <jcscom13@gmail.com>
2022-03-10 21:25:49 +0900

    Add files via upload
    test

---
+++ AroundHubSpringBootApplication.java
@@ -0,0 +1,12 @@
+package studio.thinkground.aroundhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundHubSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AroundHubSpringBootApplication.class, args);
+    }	
+}
